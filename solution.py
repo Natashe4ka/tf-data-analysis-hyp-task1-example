21d67b6e66b674d83ea4bad3f9046758c25aba76
Natashe4ka <90340759+Natashe4ka@users.noreply.github.com>
2023-04-08 17:52:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+from statsmodels.stats.proportion import proportions_ztest 
+chat_id = 965404933 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+   
+    alpha = 0.09
+    stat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='smaller')
+    if pval<=alpha: 
+        return True
+    else:
+        return False
